b931b27e843f2c3b0104806a617d18df2141694c
Dviih <dviih2s@gmail.com>
2022-11-12 11:03:14 -0300

    source of entrypoint and Dockerfile.

---
+++ src/entrypoint.sh
@@ -0,0 +1,10 @@
+mkdir -p /root/.vnc
+
+echo "%password" | tightvncpasswd -f > /root/.vnc/passwd
+chmod 0400 /root/.vnc/passwd
+
+USER=root tightvncserver :0
+USER=root tightvncserver -kill :0
+echo "cd /app/bin && ./%app.sh" >> /root/.vnc/xstartup
+USER=root tightvncserver :0 -geometry %res -depth 24
+/novnc/utils/novnc_proxy
