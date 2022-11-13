59f7d8625d064e6d77ccdd5bcf522fa458293e27
Dviih <dviih2s@gmail.com>
2022-11-12 21:41:19 -0300

    Lazy to commit all of that but improvements and addons for clipboard and so on

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,6 +5,7 @@ chmod 0400 /root/.vnc/passwd
 
 USER=root tightvncserver :0
 USER=root tightvncserver -kill :0
-echo "cd /app/bin && ./%app.sh" >> /root/.vnc/xstartup
+echo "/app/bin/%app_lower.sh" >> /root/.vnc/xstartup
+echo "autocutsel -fork" >> /root/.vnc/xstartup
 USER=root tightvncserver :0 -geometry %res -depth 24
 /novnc/utils/novnc_proxy --listen 80
